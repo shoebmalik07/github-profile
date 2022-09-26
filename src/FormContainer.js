d4f3d4f22481f98dcaea5937493d6e643dabca19
Shoeb-0204 <malikshoaib0402@gmail.com>
2022-09-27 01:40:55 +0530

    initial commit

---
+++ src/FormContainer.js
@@ -0,0 +1,21 @@
+import {Flex} from "@chakra-ui/react"
+
+import React from 'react'
+
+const FormContainer = ({children, width= 'xl'}) => {
+  return (
+    <Flex
+    direction='column'  
+    boxShadow='md'
+    rounded='md'
+    bgColor='white'
+    p = '10'
+    width ={width}
+    >
+      {children}
+    </Flex>
+    
+  )
+}
+
+export default FormContainer;
---
+++ src/LoginScreen.js
@@ -0,0 +1,85 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Link,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import FormContainer from "./FormContainer";
+import axios from 'axios'
+import Card from './Card'
+
+
+
+const LoginScreen = () => {
+  const [name, setName] = useState("");
+  const [newData, setNewData] = useState({});
+  const [show, setShow] = useState(false);
+  // const [error,setError] = useState(false)
+
+
+  const fetchData =async ()=>{
+    const {data} = await axios.get(`https://api.github.com/users/${name}`)
+    setNewData(data)
+    console.log(data)
+  //   if(!newData.length){
+  //     setShow(false)
+  //     setError(true)
+  //   }else{
+  //     setShow(true)
+  //   }
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    
+    fetchData()
+    setShow(true)
+    console.log(newData)
+    
+
+  };
+
+console.log(show)
+  return (
+    <>
+    <Flex w="full" alignItems="center" justifyContent="center" py="p">
+      <FormContainer>
+        <Heading as="h1" mb="8" fontSize="3xl">
+          Github Profile
+        </Heading>
+
+        <form onSubmit={submitHandler}>
+          <FormControl id="email">
+            <FormLabel htmlFor="email">Enter your name</FormLabel>
+            <Input
+            isRequired
+              id="name"
+              type="name"
+              placeholder="Enter your name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </FormControl>
+
+          <Spacer h="3" />
+
+          <Button type="submit" colorScheme="teal" mt="4">
+            Submit
+          </Button>
+        </form>
+      </FormContainer>
+    </Flex>
+    
+    {
+      show && (<Card data = {newData}/>)
+    }
+   </>
+  );
+};
+
+export default LoginScreen;
